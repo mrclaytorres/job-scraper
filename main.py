dd2e612ea5acc6d69404df8a3b4645e0a1d537db
Algen <adelacruz@innovuze.com>
2021-06-14 18:21:32 +0800

    User-input job search.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-print(f"Put some skill that you are not familiar with")
+print(f"What job are you looking for?")
+job_keyword = input('>')
+print(f"Put a skill that you are not familiar with")
 unfamiliar_skill = input('>')
 print(f"Filtering out unfamiliar skill...")
 
 def find_jobs():
-    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    page_to_scrape = ('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={}&txtLocation=').format(job_keyword)
+    html_text = requests.get(page_to_scrape).text
     soup = BeautifulSoup(html_text, 'lxml')
     jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
@@ -33,7 +36,7 @@ def find_jobs():
                 print(f"More Info: {more_info}")
                 print('')
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     while True:
         find_jobs()
         time_wait = 10
